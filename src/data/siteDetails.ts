f0184e88c0ab3781e6b7d9d8079141c7774cc159
Deependu Jha <deependujha21@gmail.com>
2025-03-21 19:56:39 +0530

    first commit

---
+++ src/data/siteDetails.ts
@@ -0,0 +1,12 @@
+export const siteDetails = {
+    siteName: 'trai.dev',
+    siteUrl: 'https://trai.dev',
+    metadata: {
+        title: 'trai.dev - trace + ai for developers to make sense of their profiled traces like never before',
+        description: 'trai.dev leverages AI to analyze and visualize profiled traces, helping developers gain deeper insights with ease.',
+    },
+    language: 'en-us',
+    locale: 'en-US',
+    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // Update if needed
+    googleAnalyticsId: '', // Add tracking ID if available
+}
