7dc4c778700642e4fce052ae6ac662c09b367b48
niteshKrr <nkumar35101@gmail.com>
2025-03-23 11:33:36 +0530

    work in progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Source_Sans_3, Manrope } from "next/font/google";
+import AuthProvider from "@/components/AuthProvider";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -46,12 +47,14 @@ export default function RootLayout({
       <body
         className={`${manrope.className} ${sourceSans.className} antialiased`}
       >
-        {siteDetails.googleAnalyticsId && <GoogleAnalytics gaId={siteDetails.googleAnalyticsId} />}
-        <Header />
-        <main>
-          {children}
-        </main>
-        <Footer />
+        <AuthProvider>
+          {siteDetails.googleAnalyticsId && (
+            <GoogleAnalytics gaId={siteDetails.googleAnalyticsId} />
+          )}
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
