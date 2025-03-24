d77c74c7d1e1e0f17b416c9464b65d7554c8a309
Deependu Jha <deependujha21@gmail.com>
2025-03-24 08:52:44 +0530

    update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,91 +2,91 @@
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
+import Image from "next/image";
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { HiOutlineXMark, HiBars3 } from "react-icons/hi2";
-import { FaFingerprint } from "react-icons/fa";
 
 import Container from "./Container";
 import { siteDetails } from "@/data/siteDetails";
 import { menuItems } from "@/data/menuItems";
 
 const Header: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [ isOpen, setIsOpen ] = useState( false );
   const { data: session } = useSession();
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
+    setIsOpen( !isOpen );
   };
 
   return (
     <header className="bg-transparent fixed top-0 left-0 right-0 md:absolute z-50 mx-auto w-full">
       <Container className="!px-0">
         <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
-          {/* Logo */}
+          {/* Logo */ }
           <Link href="/" className="flex items-center gap-2">
-            <FaFingerprint className="text-foreground min-w-fit w-7 h-7" />
+              <Image src="/assets/logo.png" alt="logo" height={40} width={40} />
             <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
-              {siteDetails.siteName}
+              { siteDetails.siteName }
             </span>
           </Link>
-          {/* Desktop Menu */}
+          {/* Desktop Menu */ }
           <ul className="hidden md:flex space-x-6">
-            {menuItems.map((item) => (
-              <li key={item.text}>
+            { menuItems.map( ( item ) => (
+              <li key={ item.text }>
                 <Link
-                  href={item.url}
+                  href={ item.url }
                   className="text-foreground hover:text-foreground-accent transition-colors"
                 >
-                  {item.text}
+                  { item.text }
                 </Link>
               </li>
-            ))}
+            ) ) }
 
             <li>
-              {session ? (
+              { session ? (
                 <button
-                  onClick={() => signOut()}
+                  onClick={ () => signOut() }
                   className="text-white bg-red-500 hover:bg-red-600 px-8 rounded-full"
                 >
                   Logout
                 </button>
               ) : (
                 <button
-                  onClick={() =>
-                    signIn(undefined, { callbackUrl: "/dashboard" })
+                  onClick={ () =>
+                    signIn( undefined, { callbackUrl: "/dashboard" } )
                   }
                   className="text-black bg-primary hover:bg-primary-accent px-8 rounded-full"
                 >
                   Login
                 </button>
-              )}
+              ) }
             </li>
           </ul>
 
-          {/* Mobile Menu Button */}
+          {/* Mobile Menu Button */ }
           <div className="md:hidden flex items-center">
             <button
-              onClick={toggleMenu}
+              onClick={ toggleMenu }
               type="button"
               className="bg-primary text-black focus:outline-none rounded-full w-10 h-10 flex items-center justify-center"
               aria-controls="mobile-menu"
-              aria-expanded={isOpen}
+              aria-expanded={ isOpen }
             >
-              {isOpen ? (
+              { isOpen ? (
                 <HiOutlineXMark className="h-6 w-6" aria-hidden="true" />
               ) : (
                 <HiBars3 className="h-6 w-6" aria-hidden="true" />
-              )}
+              ) }
               <span className="sr-only">Toggle navigation</span>
             </button>
           </div>
         </nav>
       </Container>
 
-      {/* Mobile Menu with Transition */}
+      {/* Mobile Menu with Transition */ }
       <Transition
-        show={isOpen}
+        show={ isOpen }
         enter="transition ease-out duration-200 transform"
         enterFrom="opacity-0 scale-95"
         enterTo="opacity-100 scale-100"
@@ -96,35 +96,35 @@ const Header: React.FC = () => {
       >
         <div id="mobile-menu" className="md:hidden bg-white shadow-lg">
           <ul className="flex flex-col space-y-4 pt-1 pb-6 px-6">
-            {menuItems.map((item) => (
-              <li key={item.text}>
+            { menuItems.map( ( item ) => (
+              <li key={ item.text }>
                 <Link
-                  href={item.url}
+                  href={ item.url }
                   className="text-foreground hover:text-primary block"
-                  onClick={toggleMenu}
+                  onClick={ toggleMenu }
                 >
-                  {item.text}
+                  { item.text }
                 </Link>
               </li>
-            ))}
+            ) ) }
             <li>
-              {session ? (
+              { session ? (
                 <button
-                  onClick={() => signOut()}
+                  onClick={ () => signOut() }
                   className="text-white bg-red-500 hover:bg-red-600 px-5 rounded-full w-fit"
                 >
                   Logout
                 </button>
               ) : (
                 <button
-                  onClick={() =>
-                    signIn(undefined, { callbackUrl: "/dashboard" })
+                  onClick={ () =>
+                    signIn( undefined, { callbackUrl: "/dashboard" } )
                   }
                   className="text-black bg-primary hover:bg-primary-accent px-5 rounded-full w-fit"
                 >
                   Login
                 </button>
-              )}
+              ) }
             </li>
           </ul>
         </div>
